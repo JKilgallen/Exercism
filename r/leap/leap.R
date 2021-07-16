63fb54c66cd842f53913991ec4987f3a5cc6e2c2
Jack Kilgallen <43824116+JKilgallen@users.noreply.github.com>
2021-07-16 16:14:07 +0100

    Updated solution to leap exercise on R track.

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -1,8 +1,4 @@
 leap <- function(year) {
 
-  if ((year %% 4 == 0 && year %% 100 != 0) || (year %% 4 == 0 && year %% 400 == 0)) {
-    TRUE
-  } else {
-    FALSE
-  }
+(year %% 4 == 0 && year %% 100 != 0) || (year %% 400 == 0)
 }
