f0065254e90526563a91b8bd7f663ac3c1e08e58
Jack Kilgallen <43824116+JKilgallen@users.noreply.github.com>
2021-09-07 15:53:41 +0100

    Downloaded files for phone number exercise in the R track.

--- r/phone-number/phone-number.R
+++ r/phone-number/phone-number.R
@@ -1,3 +1,11 @@
 parse_phone_number <- function(number_string) {
-  
+  number_characters <- strsplit(number_string, "")[[1]]
+  phone_number <- ""
+  for (i in 1:length(number_characters)) {
+    print(number_characters[length(number_characters) - i + 1])
+    if (is.numeric(number_characters[length(number_characters) - i + 1]) & length(phone_number) < 10) {
+      phone_number <- paste(number_characters[length(number_characters) - i + 1], phone_number)
+    }
+  }
+  phone_number
 }
